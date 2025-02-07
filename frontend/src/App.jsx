bcea781c0184831b0ba8d4835cab4c4515460816
Demean Vlad <demeanvla8@gmail.com>
2025-02-07 22:54:38 +0200

    createpage done || 25% work done homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
+import { useProductStore } from "./store/product";
 
 
 function App() {
+  const {products} = useProductStore();
   return (
-    <Box minHeight={"100vh"}>
-      <NavBar />
+    <Box minHeight={"100vh"} bg={useColorModeValue("gray.100", "gray.800")}>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
